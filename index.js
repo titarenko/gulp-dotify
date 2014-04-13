ab2ccaedeadb0b00da80bd98b9f6d9d54d57d561
Gilad Peleg <giladp007@gmail.com>
2014-04-13 09:27:08 +0300

    fix extension and leading dots

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const PLUGIN_NAME = 'gulp-dotify';
 
 function getTemplateName(root, name, extension, separator) {
     //get relative name from root to name
-    var relativeName = path.relative(root, name);
-    //get old extname (mainly for length)
-    var oldExt = path.extname(relativeName);
-    //now remove it and add the new one
-    relativeName = relativeName.slice(0, (-oldExt.length || relativeName.length)) + extension;
+    var relativeName = path.normalize(path.relative(root, name));
+    //clean possible leading dots
+    relativeName = relativeName.replace('../', '');
+    //remove extension
+    relativeName = path.basename(relativeName, extension);
     //return new template name with separator
     return relativeName.replace('/', separator);
 }
